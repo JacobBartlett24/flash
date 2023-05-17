fde038b46b43b9f91fdad780ae83dc359bd55391
JacobBartlett24 <jacobart@ttu.edu>
2023-05-17 12:36:50 -0500

    Generate types for supabase db

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { LoaderArgs, LoaderFunction, V2_MetaFunction } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
 import styles from "~/styles/LandingPage.css";
 import skull from "../../public/flameSkull.png";
-import {  Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import supabase from "utils/supabase";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
@@ -12,11 +13,21 @@ export const meta: V2_MetaFunction = () => {
   return [{ title: "Flash" }];
 };
 
+export const loader: LoaderFunction = async ({ }: LoaderArgs) => {
+  const { data } = await supabase.from('test').select();
+
+  return { data }
+}
+
 export default function Index() {
+
+  const { data } = useLoaderData()
+
   return (
     <div style={{ fontFamily: "Bebas Neue, sans-serif", lineHeight: "1.4" }}>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <div className="topSection">
-        <img src={skull} alt="Flaming Skull"/>
+        <img src={skull} alt="Flaming Skull" />
         <h1>Flash ⚡</h1>
         <p>
           The best way to showcase your flash on all social media platforms.
