2beac814954ed9e17b777280794f501793b368b8
JacobBartlett24 <jacobart@ttu.edu>
2023-06-01 20:25:55 -0500

    Add most signup methods

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,42 +1,65 @@
-import {
-  LinksFunction,
-  ActionArgs,
-  ActionFunction,
-  json,
-  LoaderArgs,
-} from '@remix-run/node'
-import { Form, Link, useLoaderData, useOutletContext } from '@remix-run/react'
-import {
-  createBrowserClient,
-  createServerClient,
-} from '@supabase/auth-helpers-remix'
-import { SupabaseClient, createClient } from '@supabase/supabase-js'
-import { useState } from 'react'
+import type { LinksFunction } from '@remix-run/node'
+import type { SupabaseOutletContext } from '~/root'
+import { FcGoogle } from 'react-icons/fc'
+
+import skull from '../../public/flameSkull.png'
+import { Form, useOutletContext } from '@remix-run/react'
 import styles from '~/styles/loginSignup.css'
+import { FaDiscord, FaGithub } from 'react-icons/fa'
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }]
 }
 
 export default function Signup() {
+  const { supabase } = useOutletContext<SupabaseOutletContext>()
+
+  async function signInWithGoogle() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    })
+  }
+
+  async function signInWithDiscord() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'discord',
+    })
+  }
+
+  async function signInWithGithub() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    })
+  }
+
   return (
     <div className="page">
       <div className="card">
-        <h1>Signup</h1>
-        <Form className="buttonGroup" method="post">
-          <input
-            className="userInput"
-            name="username"
-            placeholder="Username..."
-          />
-          <input
-            className="userInput"
-            name="password"
-            placeholder="Password..."
-          />
-          <button type="button">Sign Up</button>
-        </Form>
-        <Link to="/homepage">Cheater</Link>
+        <div className="imgWrapper">
+          <img src={skull} alt="flamingskull"></img>
+        </div>
+        <div className="formWrapper">
+          <Form className="signupForm" method="post">
+            <input
+              className="userInput"
+              name="username"
+              placeholder="Username..."
+            />
+            <input
+              className="userInput"
+              name="password"
+              placeholder="Password..."
+            />
+            <button className="button" type="submit">
+              Sign up
+            </button>
+            <div className="signupIcons">
+              <FcGoogle onClick={signInWithGoogle} size={30} />
+              <FaGithub onClick={signInWithGithub} size={30} />
+              <FaDiscord onClick={signInWithDiscord} size={30} />
+            </div>
+          </Form>
+        </div>
       </div>
     </div>
   )
